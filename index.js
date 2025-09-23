ba73cd75ab0d8cc65f981ce57c0bd6f386655882
danameisnothing <123193945+danameisnothing@users.noreply.github.com>
2025-09-23 07:32:38 +0000

    general update

--- index.js
+++ index.js
@@ -1,31 +1,91 @@
 // The Temporal API is still widely unsupported
 const W10_EOL_DATE = new Date(2025, 9, 14);
+const PRIDE_MONTH = 6 - 1;
 
 let win10EOLEl = document.querySelector("#windows_10_eol_time");
-let delta = W10_EOL_DATE.getTime() - Date.now()
+let prideStatEl = document.querySelector("#pride_stat");
 
-// yikes
-let seconds = Math.floor(delta / 1000);
-let minutes = Math.floor(seconds / 60);
-let hours = Math.floor(minutes / 60);
-let days = Math.floor(hours / 24);
-let weeks = Math.floor(days / 7);
+function jankDeltaData(delta) {
+    // yikes
+    let seconds = delta / 1000;
+    let minutes = seconds / 60;
+    let hours = minutes / 60;
+    let days = hours / 24;
+    let weeks = days / 7;
+    let months = weeks / 4;
+    let years = months / 12;
 
-let builder = "";
-
-if (seconds >= 0) {
-    builder = "will reach EOL in ";
-    if (weeks > 0) {
-        builder += `${weeks} week${(weeks != 1) ? "s" : ""}, `;
-    } if (days > 0) {
-        builder += `${days % 7} day${(days % 7 != 1) ? "s" : ""}, `;
-    } if (hours > 0) {
-        builder += `${hours % 24} hour${(hours % 24 != 1) ? "s" : ""}, `;
-    } if (minutes > 0) {
-        builder += `${minutes % 60} minute${(minutes % 60 != 1) ? "s" : ""}, `;
-    }
-    builder += `${seconds % 60} second${(seconds % 60 != 1) ? "s" : ""}`;
-} else {
-    builder = "has reached EOL :(";
+    return {
+        "seconds_raw": seconds,
+        "minutes_raw": minutes,
+        "hours_raw": hours,
+        "days_raw": days,
+        "weeks_raw": weeks,
+        "months_raw": months,
+        "years_raw": years,
+        "seconds": Math.floor(seconds % 60),
+        "minutes": Math.floor(minutes % 60),
+        "hours": Math.floor(hours % 24),
+        "days": Math.floor(days % 7),
+        "weeks": Math.floor(weeks % 4),
+        "months": Math.floor(months % 12),
+        "years": Math.floor(years)
+    };
 }
-win10EOLEl.innerText = `${builder}`;
+
+function jankDeltaFormat(data) {
+    let builder = "";
+    if (data["months"] > 0) {
+        builder += `${data["months"]} month${(data["months"] != 1) ? "s" : ""}, `;
+    } if (data["weeks"] > 0) {
+        builder += `${data["weeks"]} week${(data["weeks"] != 1) ? "s" : ""}, `;
+    } if (data["days"] > 0) {
+        builder += `${data["days"]} day${(data["days"] != 1) ? "s" : ""}, `;
+    } if (data["hours"] > 0) {
+        builder += `${data["hours"]} hour${(data["hours"] != 1) ? "s" : ""}, `;
+    } if (data["minutes"] > 0) {
+        builder += `${data["minutes"]} minute${(data["minutes"] != 1) ? "s" : ""}, `;
+    }
+    builder += `${data["seconds"]} second${(data["seconds"] != 1) ? "s" : ""}`;
+
+    return builder;
+}
+
+function getWindows10Status() {
+    let delta = W10_EOL_DATE.getTime() - new Date().getTime();//Date.now();
+
+    let data = jankDeltaData(delta);
+
+    let builder = "";
+
+    // no sprintf?
+    if (data["seconds_raw"] > 0) {
+        builder = "will reach EOL in ";
+        builder += jankDeltaFormat(data);
+    } else {
+        builder = "has reached EOL :(";
+    }
+
+    return builder;
+}
+
+function getPrideStatus() {
+    let builder = "";
+    if (new Date().getMonth() == PRIDE_MONTH) {
+        builder = "Pride month is currently ongoing, ending in NULL";
+    } else {
+        let isUnder = new Date().getMonth() < PRIDE_MONTH; // last is prev year
+        let isOver = new Date().getMonth() > PRIDE_MONTH; // last is now
+
+        // last
+        let dec = (isUnder) ? new Date(new Date().getFullYear()-1, PRIDE_MONTH, 1) : new Date(new Date().getFullYear(), PRIDE_MONTH, 1);
+        let deltaLast = Date.now() - dec.getTime();
+        let last = jankDeltaFormat(jankDeltaData(deltaLast));
+        builder = `It has been ${last} since Pride Month ended, and NULL until next one?`;
+    }
+
+    return builder;
+}
+
+win10EOLEl.innerText = `${getWindows10Status()}`;
+prideStatEl.innerText = `${getPrideStatus()}`;
